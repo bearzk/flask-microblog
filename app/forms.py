423793484feae26f226222d0de5bcf6f5bfec54f
bearzk <kai.zhangxa@gmail.com>
2014-04-06 17:46:24 +0200

    PEP8 and typo fix, added import models.py

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField, TextAreaField
 from wtforms.validators import Required, Length
+from app.models import User
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
@@ -15,7 +16,7 @@ class EditForm(Form):
     about_me = TextAreaField('about_me', validators=[Length(min=0,max=140)])
 
     def __init__(self, original_nickname, *args, **kwargs):
-        From.__init__(self, *args, **kwargs)
+        Form.__init__(self, *args, **kwargs)
         self.original_nickname = original_nickname
 
     def validate(self):
@@ -23,8 +24,8 @@ class EditForm(Form):
             return False
         if self.nickname.data == self.original_nickname:
             return True
-        user = User.query.filter_by(nickname = self.nickname.data).first()
-        if user != None:
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user is not None:
             self.nickname.errors.append('This nickname is already in use. Please choose another one.')
             return False
         return True
